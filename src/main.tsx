f92197dd6206258b122f7027b7b4f3c5f5d99f15
Ryan Nograles <125443916+ryan-nograles@users.noreply.github.com>
2023-03-16 16:59:05 +0800

    added tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "~/components";
 
 import "./i18n";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
